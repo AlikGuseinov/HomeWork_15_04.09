a1c88cadf9b1b9e6335421797063b02754300f8b
Alik <9774952689@bk.ru>
2022-09-04 01:11:22 +0300

    Completed15Task

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a==6 || a==7)
+{
+    Console.WriteLine("Это выходной, можешь отдыхать)");
+}
+else
+{
+    Console.WriteLine("Это не выходной, работай!");
+}
